58c62585cc65d3dcaa4efeb0940e8cd8c4f46fa8
Matthew Hinton <mhinton@gmail.com>
2018-06-08 11:14:10 -0500

    Fix the favorite prop

--- src/store.js
+++ src/store.js
@@ -4,7 +4,49 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {}
+  strict: process.env.NODE_ENV !== "production",
+  state: {
+    entity: {
+      icon: "fa-align-justify",
+      title: "Nihil sed accusamus aliquam",
+      // body: "Rem earum quod quo optio impedit temporibus nihil harum. Corporis quis quia occaecati qui culpa et aliquid. Quia qui eius id ut labore quis error."
+      properties: [
+        {
+          propId: "Jo4Dg8JXkrnzX6nbC",
+          label: "color",
+          value: "dark-blue",
+          type: "button"
+        },
+        {
+          propId: "H9ho3xBaDWmDYA4in",
+          label: "favorite",
+          value: false,
+          type: "favorite"
+        },
+        {
+          propId: "JeA7oug2QpqYTqAec",
+          label: "enabled",
+          value: true,
+          type: "button"
+        }
+      ]
+    }
+  },
+
+  mutations: {
+    SET_PROP_VALUE: function(state, propUpdate) {
+      // const prop = this.state.entity.find(p => p.propId === propUpdate.propId);
+      propUpdate.prop.value = propUpdate.value;
+      return true;
+    }
+  },
+
+  actions: {
+    updateProp({ commit, state }, data) {
+      const prop = state.entity.properties.find(p => p.propId === data.propId);
+      if (prop) {
+        commit("SET_PROP_VALUE", { prop: prop, value: data.value });
+      }
+    }
+  }
 });
