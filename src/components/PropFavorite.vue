0f16e68024f62a4013ef3d090b7538b0cd03b992
Matthew Hinton <mhinton@gmail.com>
2018-06-08 11:13:26 -0500

    Configure fontawesome to leave the i tag and embed the svg in it. Figured out how to toggle the icon prefix.

---
+++ src/components/PropFavorite.vue
@@ -0,0 +1,37 @@
+<template>
+  <span @click="toggle">
+    <i class="fa-star" :class="iconClass()"></i>
+  </span>
+</template>
+
+<script>
+export default {
+  props:["value"],
+
+  computed: {
+    favorited() {
+      const val = this.$props.value;
+      const prop = this.$store.state.entity.properties.find(p => p.propId === val.propId);
+      return prop.value;
+    }
+  },
+
+  methods: {
+    toggle() {
+      const val = this.$props.value;
+      this.$store.dispatch("updateProp", { propId: val.propId, value: !val.value });
+    },
+
+    iconClass() {
+      if (this.favorited) {
+        return 'fas';
+      } else {
+        return 'far';
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
