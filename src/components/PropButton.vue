58df2d61cb6fb09ddadf6136be8adfc947cb1114
Matthew Hinton <mhinton@gmail.com>
2018-06-08 11:16:42 -0500

    Receive value prop

---
+++ src/components/PropButton.vue
@@ -0,0 +1,16 @@
+<template>
+  <button
+    class="bg-blue hover:bg-blue-dark text-white font-bold py-2 px-4 rounded-full"
+    @click="$emit('click-prop', { propId: value.propId })"
+  >{{value.value}}</button>
+</template>
+
+<script>
+export default {
+  props:["value"],
+}
+</script>
+
+<style scoped>
+
+</style>
