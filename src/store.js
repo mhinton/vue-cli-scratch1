2f47e398d520ebac2088efc463a9fb7d72713a24
Matthew Hinton <mhinton@gmail.com>
2018-06-11 11:56:00 -0500

    Add propOptions and action/mutation

--- src/store.js
+++ src/store.js
@@ -13,29 +13,82 @@ export default new Vuex.Store({
       properties: [
         {
           propId: "Jo4Dg8JXkrnzX6nbC",
+          type: "button",
           label: "color",
-          value: "dark-blue",
-          type: "button"
+          value: "#2779bd",
+          text: "blue-dark",
+          render: "Color"
         },
         {
           propId: "H9ho3xBaDWmDYA4in",
           label: "favorite",
           value: false,
-          type: "favorite"
+          type: "favorite",
+          render: "Favorite"
         },
         {
           propId: "JeA7oug2QpqYTqAec",
           label: "enabled",
           value: true,
-          type: "button"
+          text: true,
+          type: "button",
+          render: "Boolean"
         }
       ]
-    }
+    },
+    propOptions: [
+      {
+        id: "TEDvDt3GoKtYuPEmc",
+        render: "Color",
+        text: "blue-darkest",
+        value: "#12283a"
+      },
+      {
+        id: "wg3DbqErBYJPY54bW",
+        render: "Color",
+        text: "blue-darker",
+        value: "#1c3d5a"
+      },
+      {
+        id: "8azR88gDg8BGep5rz",
+        render: "Color",
+        text: "blue-dark",
+        value: "#2779bd"
+      },
+      {
+        id: "679QmDyt6YvQ9PuFF",
+        render: "Color",
+        text: "blue",
+        value: "#3490dc"
+      },
+      {
+        id: "PbaHqd2WLK2sGWP4F",
+        render: "Color",
+        text: "blue-light",
+        value: "#6cb2eb"
+      },
+      {
+        id: "mW9548nyxj7DdnErg",
+        render: "Color",
+        text: "blue-lighter",
+        value: "#bcdefa"
+      },
+      {
+        id: "xJZN3Q6WATjeCRujc",
+        render: "Color",
+        text: "blue-lightest",
+        value: "#eff8ff"
+      }
+    ]
   },
 
   mutations: {
     SET_PROP_VALUE: function(state, propUpdate) {
       propUpdate.prop.value = propUpdate.value;
+      return true;
+    },
+    SET_PROP_TEXT: function(state, propUpdate) {
+      propUpdate.prop.text = propUpdate.text;
       return true;
     }
   },
@@ -45,6 +98,9 @@ export default new Vuex.Store({
       const prop = state.entity.properties.find(p => p.propId === data.propId);
       if (prop) {
         commit("SET_PROP_VALUE", { prop: prop, value: data.value });
+        if (prop.text) {
+          commit("SET_PROP_TEXT", { prop: prop, text: data.text });
+        }
       }
     }
   }
