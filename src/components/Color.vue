44c808086421d01df88d5b2caf0f620974cf36f4
Matthew Hinton <mhinton@gmail.com>
2018-06-11 11:56:22 -0500

    Add color options component

---
+++ src/components/Color.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <div v-for="item in items"
+        :key="item.id"
+        @click="pick"
+        :style="`background-color: ${item.value}`">
+      <span :data-id="item.id">{{ item.text }}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      items: this.$options.propsData.items
+    }
+  },
+
+  methods: {
+    pick(evt) {
+      const id = evt.target.dataset.id;
+      this.choose(id);
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
