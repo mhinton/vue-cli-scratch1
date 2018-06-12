5c80e34f189b362af2a2d0e27301d82ca3f66c47
Matthew Hinton <mhinton@gmail.com>
2018-06-12 09:44:53 -0500

    Revert to local file

--- src/App.vue
+++ src/App.vue
@@ -13,8 +13,7 @@ export default {
   head: {
     script: [
       // <!--load everything-->
-      // { type: "text/javascript", src: "/fontawesome-all.js", defer: true }
-      { type: "text/javascript", src: "https://use.fontawesome.com/releases/v5.0.13/js/all.js", integrity:"sha384-xymdQtn1n3lH2wcu0qhcdaOpQwyoarkgLVxC/wZ5q7h9gHtxICrpcaSUfygqZGOe", crossorigin: "anonymous" }
+      { type: "text/javascript", src: "/fontawesome-all.js", defer: true }
     ]
   }
 };
