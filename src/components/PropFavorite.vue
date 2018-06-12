f7fe83202066722769ab582e1258c56001564e62
Matthew Hinton <mhinton@gmail.com>
2018-06-12 09:43:42 -0500

    Use iconClass method to set value

--- src/components/PropFavorite.vue
+++ src/components/PropFavorite.vue
@@ -1,28 +1,31 @@
 <template>
   <span @click="toggle">
-    <i class="fa-star" :class="iconClass()"></i>
+    <i :class="iconClass()" class="fa-star"></i>
   </span>
 </template>
 
 <script>
 export default {
-  props:["property"],
+  props: ["property"],
 
   methods: {
     toggle() {
       const property = this.$props.property;
-      this.$store.dispatch("updateProp", { propId: property.propId, value: !property.value });
+      this.$store.dispatch("updateProp", {
+        propId: property.propId,
+        value: !property.value
+      });
     },
 
     iconClass() {
       if (this.property.value) {
-        return 'fas';
+        return "fas";
       } else {
-        return 'far';
+        return "far";
       }
     }
   }
-}
+};
 </script>
 
 <style scoped>
