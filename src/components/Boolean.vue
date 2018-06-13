a03da79b8fbfc10f4ba864b63fa7dfb83ea5637c
Matthew Hinton <mhinton@gmail.com>
2018-06-13 16:14:28 -0500

    Add

---
+++ src/components/Boolean.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="picker-boolean">
+    <div v-for="item in items"
+        :key="item.id"
+        :data-id="item.id"
+        class="boolean-item"
+        @click="pick">
+      {{ item.text }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      items: this.$options.propsData.items
+    }
+  },
+
+  methods: {
+    pick(evt) {
+      const id = evt.target.dataset.id;
+      this.choose(id);
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.picker-boolean {
+  cursor: pointer;
+  padding: 0;
+  margin: 0;
+}
+</style>
